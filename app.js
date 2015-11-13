16ab7667d0094231337a94adfca3f24c40c64b4f
ybinstock <ybinstock@gmail.com>
2015-11-13 00:05:24 -0800

    ten

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-server.listen(process.env.PORT || 3000, function() {
+server.listen(process.env.PORT || 80, function() {
   console.log('server started');
 });
